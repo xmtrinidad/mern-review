a3e209b5b6bca9adf32a39d9502555af75d593ec
Xavier <xmtrinidad@gmail.com>
2022-09-17 11:30:50 -0700

    added routes and mongo

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
 require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const workoutRouters = require('./routes/workouts');
+
 const app = express();
 
-// middleware
+
+// Middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // Routes
-app.get('/', (req, res) => {
-  res.json({msg: 'welcome to the app'});
-});
+app.use('/api/workouts/', workoutRouters);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`)
-});
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`)
+    });
+  })
+  .catch((error) => {
+    console.log(error)
+  });
