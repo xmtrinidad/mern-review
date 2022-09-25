e0272eab587991b02d6cebd53521c52a816112af
Xavier <xmtrinidad@gmail.com>
2022-09-25 14:58:21 -0700

    added context

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,68 +1,70 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
+const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext()
 
-const WorkForm = () => {
-  const [ title, setTitle ] = useState('');
-  const [ load, setLoad ] = useState('');
-  const [ reps, setReps ] = useState('');
-  const [error, setError] = useState(null);
+  const [title, setTitle] = useState('')
+  const [load, setLoad] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     const workout = {title, load, reps}
-
+    
     const response = await fetch('/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
         'Content-Type': 'application/json'
       }
-    });
-
-    const json = await response.json();
+    })
+    const json = await response.json()
 
     if (!response.ok) {
-      setError(json.error);
+      setError(json.error)
     }
     if (response.ok) {
-      setTitle('');
-      setLoad('');
+      setError(null)
+      setTitle('')
+      setLoad('')
       setReps('')
-      setError(null);
-      console.log('new workout added', json)
+      dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
-  };
+
+  }
 
   return (
-    <form className="create" onSubmit={handleSubmit}>
-      <h3>Add a new workout</h3>
+    <form className="create" onSubmit={handleSubmit}> 
+      <h3>Add a New Workout</h3>
 
-      <label>Exercise Title:</label>
+      <label>Excersize Title:</label>
       <input 
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
+        type="text" 
+        onChange={(e) => setTitle(e.target.value)} 
         value={title}
       />
 
-      <label>Load (in lbs)</label>
+      <label>Load (in kg):</label>
       <input 
-        type="number"
-        onChange={(e) => setLoad(e.target.value)}
+        type="number" 
+        onChange={(e) => setLoad(e.target.value)} 
         value={load}
       />
 
-      <label>Reps:</label>
+      <label>Number of Reps:</label>
       <input 
-        type="number"
-        onChange={(e) => setReps(e.target.value)}
-        value={reps}
+        type="number" 
+        onChange={(e) => setReps(e.target.value)} 
+        value={reps} 
       />
 
-      <button>Add workout</button>
+      <button>Add Workout</button>
       {error && <div className="error">{error}</div>}
     </form>
   )
-};
+}
 
-export default WorkForm;
+export default WorkoutForm
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,34 +1,36 @@
-import { useEffect, useState } from "react";
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkForm from "../components/WorkoutForm";
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
-  
-  // Empty array makes this fire once
+  const { workouts, dispatch } = useWorkoutsContext()
+
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts');
-      const json = await response.json();
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
       if (response.ok) {
-        setWorkouts(json)
+        dispatch({type: 'SET_WORKOUTS', payload: json})
       }
     }
 
-    fetchWorkouts();
-  }, []);
+    fetchWorkouts()
+  }, [dispatch])
 
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map((workout) => (
-          <WorkoutDetails key={workout._id} workout={workout} />
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
-      <WorkForm />
+      <WorkoutForm />
     </div>
   )
-};
+}
 
-export default Home;
+export default Home
